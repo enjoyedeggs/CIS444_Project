3366dac2e8e97185137a59f8f0e9242346d7ac7a
qtrong1011 <58069448+qtrong1011@users.noreply.github.com>
2020-10-24 11:28:34 -0700

    main/home page dynamic

--- main.js
+++ main.js
@@ -10,35 +10,70 @@ var mm = date.getMonth();
 var yyyy = date.getFullYear();
 var today = mm + "-" + dd + "-" + yyyy;
 function writeTable(){
-    document.write("<div class=\"forum-div\">\n" +
-        "\t\t\t<div class=\"table-format\">\n" +
-        "\t\t\t\t<div class=\"item-forum\">Forum</div>\n" +
-        "\t\t\t\t<div class=\"item-post\">Posts</div>\n" +
-        "\t\t\t\t<div class=\"item-date\">Date</div>\n" +
-        "\t\t\t</div>");
-    for(let i=0; i < courseName.length; i++){
-        document.write("<div class=\"table-format\">\n" +
-            "\t\t\t\t<div class=\"course-title\">" + courseName[i] +
-            "</div></div>")
-        for(let j = 0; j < subForumName.length; j++){
-            document.write("<div class=\"table-format\">\n" +
-                "\t\t\t\t<div class=\"item-forum item-category\">\n" +
-                "\t\t\t\t\t<div class=\"sub-icon\">\n" +
-                "\t\t\t\t\t\t<i class=\"fas fa-book-open\"></i>\n" +
-                "\t\t\t\t\t</div>\n" +
-                "\t\t\t\t\t<div class=\"sub-title\">\n" +
-                "\t\t\t\t\t\t<a class=\"sub-heading\" href=\"main.html\">" + subForumName[j]+
-                "</a>" +
-                "<div class=\"sub-description\">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>\n" +
-                "\t\t\t\t\t\t<!--it will dynamically display the description of each sub-forum -->\n" +
-                "\t\t\t\t\t</div></div>" +
-                "<div class=\"item-post\">" + numPost +
-                "</div>" +
-                "<div class=\"item-date\">" + today + "</div>" +
-                "</div>");
-        }
+    // Create table-format div
+    var table_format_heading = document.createElement("div");
+    table_format_heading.setAttribute("class","table-format");
+    document.getElementById("forum-div").appendChild(table_format_heading);
+    // Create Forum column
+    var forum_col = document.createElement("div");
+    forum_col.setAttribute("class","item-forum");
+    forum_col.innerHTML = "Forum";
+    table_format_heading.appendChild(forum_col);
+    //Create Post column
+    var post_col = document.createElement("div");
+    post_col.setAttribute("class","item-post");
+    post_col.innerHTML = "Post";
+    table_format_heading.appendChild(post_col);
+    //Create Date Column
+    var date_col = document.createElement("div");
+    date_col.setAttribute("class","item-date");
+    date_col.innerHTML = "Date";
+    table_format_heading.appendChild(date_col);
 
+    for(let i = 0; i<courseName.length;i++){
+        var table_format_courseName = document.createElement("div");
+        table_format_courseName.setAttribute("class", "table-format");
+       document.getElementById("forum-div").appendChild(table_format_courseName);
+        var course_node = document.createElement("div");
+        course_node.setAttribute("class", "course-title");
+        course_node.innerHTML = courseName[i];
+        table_format_courseName.appendChild(course_node);
+        for(let j = 0; j<subForumName.length;j++){
+            var table_format_subForum = document.createElement("div");
+            table_format_subForum.setAttribute("class", "table-format");
+            document.getElementById("forum-div").appendChild(table_format_subForum);
+            var subForum_node = document.createElement("div");
+            subForum_node.setAttribute("class","item-forum item-category")
+            table_format_subForum.appendChild(subForum_node);
+            var sub_icon_node = document.createElement("div");
+            sub_icon_node.setAttribute("class", "sub-icon");
+            subForum_node.appendChild(sub_icon_node);
+            var icon = document.createElement("i");
+            icon.setAttribute("class", "fas fa-book-open");
+            sub_icon_node.appendChild(icon);
+            var sub_title_node = document.createElement("div");
+            sub_title_node.setAttribute("class", "sub-title");
+            subForum_node.appendChild(sub_title_node);
+            var sub_heading_node = document.createElement("a");
+            sub_heading_node.setAttribute("class", "sub-heading");
+            sub_heading_node.setAttribute("href", "homework.html");
+            sub_heading_node.innerHTML = subForumName[j];
+            sub_title_node.appendChild(sub_heading_node);
+            var sub_description_node = document.createElement("div");
+            sub_description_node.setAttribute("class", "sub-description");
+            sub_description_node.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            sub_title_node.appendChild(sub_description_node);
+            var post_col_node = document.createElement("div");
+            post_col_node.setAttribute("class", "item-post");
+            post_col_node.innerHTML = "" + numPost;
+            table_format_subForum.appendChild(post_col_node);
+            var date_col_node = document.createElement("div");
+            date_col_node.setAttribute("class", "item-date");
+            date_col_node.innerHTML = today;
+            table_format_subForum.appendChild(date_col_node);
+        }
     }
-    document.write("</div>");
+
+
 }
 writeTable();
