3226e72ed9d7db893461173f94e274246242abd1
kapursuchi <54446828+kapursuchi@users.noreply.github.com>
2020-10-25 14:12:30 -0700

    added form + required fields for new_post

--- new_post.js
+++ new_post.js
@@ -1,26 +1,14 @@
-/*
 function submission() {
-	//TODO: log user out.
-// Get the input field
-var input = document.getElementById("subButton");
+	//TODO: Create new post/reply in database
 
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === enter) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("subButton").click();
-  }
-});
-
-	window.location.href = "subforum.html";
+	redirect();
+	return false;
 }
-*/
+
 var forumName;
 var course;
 var subforum;
+
 function redirect()
 {
 	forumName = (window.location).toString();
