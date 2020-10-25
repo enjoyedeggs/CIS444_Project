f8669009514f0db7a076229b974bcf74ab557a17
Shane Ghuste <sghuste@epsilonsystems.com>
2020-10-24 19:43:12 -0700

    Updated subForum html/js

--- subforum.js
+++ subforum.js
@@ -11,42 +11,45 @@ function retrieveInformation(forumName) {
 function getPosts(forumname) {
 
     var posts = new Array(); //placeholder for PHP function
-	//posts = [["User1", "Post Title", "postid1", "4", "6", "12/25/10"],["User2", "Post Title", "postid2",  "4","5", "10/1/20"],
-    //["User3", "Post Title","postid3",  "3","4", "1/4/98"]];
+	// posts = [["User1", "Post Title", "postid1", "4", "6", "12/25/10"],["User2", "Post Title", "postid2",  "4","5", "10/1/20"],
+    // ["User3", "Post Title","postid3",  "3","4", "1/4/98"]];
 	var forum = document.getElementById("forum-name");
 	forum.innerHTML = "Course Forum";
 	if (posts.length == 0) {
-		
         var divElem0 = document.createElement("div");
-		divElem0.setAttribute("class", "table-sub-format");
-		var divElem = document.createElement("div");
-		divElem.setAttribute("class", "item-subforum");
-        var divElem2 = document.createElement("div");
-        divElem2.setAttribute("class", "item-subforum-author");
-        var divElemA = document.createElement("a");
-        divElemA.setAttribute("class", "item-post-title");
-        divElemA.setAttribute("href", "main.html");
-        divElemA.innerHTML = "Post Title";
-        var divElem3 = document.createElement("div");
-        divElem3.setAttribute("class", "subforum-author");
-        divElem3.innerHTML = "Author";
-        var divElem4 = document.createElement("div");
-        divElem4.setAttribute("class", "item-replies");
-        divElem4.innerHTML = "0";
-        //var divElem5 = document.createElement("div");
-        //divElem5.setAttribute("class", "item-views");
-        //divElem5.innerHTML = "0";
-        var divElem6 = document.createElement("div");
-        divElem6.setAttribute("class", "item-last-post");
-        divElem6.innerHTML = "0";
+        divElem0.setAttribute("class", "table-sub-format");
+	    divElem0.innerHTML = "No posts to show, click \"New Post\" to be the first!";
+        
+        // var divElem0 = document.createElement("div");
+		// divElem0.setAttribute("class", "table-sub-format");
+		// var divElem = document.createElement("div");
+		// divElem.setAttribute("class", "item-subforum");
+        // var divElem2 = document.createElement("div");
+        // divElem2.setAttribute("class", "item-subforum-author");
+        // var divElemA = document.createElement("a");
+        // divElemA.setAttribute("class", "item-post-title");
+        // divElemA.setAttribute("href", "main.html");
+        // divElemA.innerHTML = "Post Title";
+        // var divElem3 = document.createElement("div");
+        // divElem3.setAttribute("class", "subforum-author");
+        // divElem3.innerHTML = "Author";
+        // var divElem4 = document.createElement("div");
+        // divElem4.setAttribute("class", "item-replies");
+        // divElem4.innerHTML = "0";
+        // var divElem5 = document.createElement("div");
+        // divElem5.setAttribute("class", "item-views");
+        // divElem5.innerHTML = "0";
+        // var divElem6 = document.createElement("div");
+        // divElem6.setAttribute("class", "item-last-post");
+        // divElem6.innerHTML = "0";
 
-        divElem0.appendChild(divElem)
-        divElem.appendChild(divElem2);
-        divElem2.appendChild(divElemA);
-        divElem2.appendChild(divElem3);
-        divElem0.appendChild(divElem4);
-        //divElem0.appendChild(divElem5);
-        divElem0.appendChild(divElem6);
+        // divElem0.appendChild(divElem)
+        // divElem.appendChild(divElem2);
+        // divElem2.appendChild(divElemA);
+        // divElem2.appendChild(divElem3);
+        // divElem0.appendChild(divElem4);
+        // divElem0.appendChild(divElem5);
+        // divElem0.appendChild(divElem6);
 
         document.getElementById("subforum-div").appendChild(divElem0);
 		
