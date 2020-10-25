566481c43014e2ad65f351f06ca573e4690258dd
kapursuchi <54446828+kapursuchi@users.noreply.github.com>
2020-10-25 14:46:49 -0700

    Update new post

--- new_post.js
+++ new_post.js
@@ -1,3 +1,30 @@
+function checkForum() {
+	forumName = (window.location).toString();
+	var pos = forumName.search(/\?/);
+	if (pos > 0){
+		var coursePos = forumName.search(/course=/);
+		var subforumPos = forumName.search(/subforum=/);
+		course = forumName.substring(coursePos+7, subforumPos);
+		subforum = forumName.substring(subforumPos+9);
+		forumName = course + " " + subforum;
+		var courseElem = document.getElementById("course-name");
+		courseElem.innerHTML= course;
+		
+		var topicElem = document.getElementById("course-forum");
+		topicElem.innerHTML= subforum;
+		
+		var doc = document.getElementsByTagName("html")[0];
+		doc.style.visibility="visible";
+		
+		
+	}
+	else {
+		//document.write("404: Page not found", "<br />", "Cannot access this page. You will be redirected to the main page.");
+		alert("Cannot access this page without selecting a forum on the main page. You will be redirected to the main page.");
+		window.location.href = "main.html";
+	}	
+}
+
 function submission() {
 	//TODO: Create new post/reply in database
 
