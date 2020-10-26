02e7a43c0071a7727bf7b9215954524e9238cfbf
kapursuchi <54446828+kapursuchi@users.noreply.github.com>
2020-10-25 17:50:36 -0700

    Fixed Nav Bars + Profile

--- view_profile.js
+++ view_profile.js
@@ -1,25 +1,58 @@
 function retrieveInformation() {
+	displayProfilePicture();
+	//displayInfo("profile-picture-div", getProfilePicture());
+	displayUserInfo();
+	//displayInfo("user-info", getUserInfo());
+	displayInfo("signature", getSignature());
+    displayInfo("courses", getCourses());
     //Get list of users to display on admin homepage
-    displayInfo("profile_picture", getProfilePicture());
+    /*displayInfo("profile_picture", getProfilePicture());
     displayInfo("first_name", getFirstName());
     displayInfo("last_name", getLastName());
     displayInfo("email_address", getEmailAddress());
     displayInfo("signature", getSignature());
-    displayInfo("courses", getCourses());
+    displayInfo("courses", getCourses());*/
 	return false;
 }
 
 function displayInfo(piece, info) {
 	var piece_Display = document.createElement("div");
-    piece_Display.setAttribute("class", "sub-description");
+    //piece_Display.setAttribute("class", "small-font");
     var innerP = document.createElement("p");
     innerP.innerHTML = info;
     piece_Display.appendChild(innerP);
     document.getElementById(piece).appendChild(piece_Display);
 }
+function displayProfilePicture() {
+	var filename = getProfilePicture(); //placeholder
+    var innerP = document.createElement("img");
+    innerP.setAttribute("src", filename);
+	innerP.setAttribute("alt", "Profile Picture");
+	innerP.setAttribute("class", "profile-picture floatleft");
+    document.getElementById("profile-picture-div").appendChild(innerP);
+}
 
+function displayUserInfo() {
+	var fname = getFirstName();
+	var lname = getLastName();
+	var email = getEmailAddress();
+	var dom = document.getElementById("user-info");
+	
+	var fnameElem = document.createElement("div");
+	fnameElem.innerHTML = "Name: " + fname + " " + lname;
+	//fnameElem.setAttribute("class", "small-font");
+	
+	var emailElem = document.createElement("div");
+	emailElem.innerHTML = "Email: " + email;
+	//emailElem.setAttribute("class", "small-font");
+	
+	dom.appendChild(fnameElem);
+	dom.appendChild(emailElem);
+	
+	
+}
 function getProfilePicture(){
-    var profile_picture = "Display the profile picture";
+    var profile_picture = "images/cr_logo.png";//placeholder for PHP function 
     return profile_picture;
 }
 
