3e77c1aebf6e53c85313e55a074891da5940c307
kapursuchi <54446828+kapursuchi@users.noreply.github.com>
2020-11-18 16:54:34 -0800

    Updated main.js format

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 /*THIS JS IS USED FOR DYNAMICALLY GENERATE THE CONTENTS OF EACH COURSE*/
 //TODO: Get the number of courses from database for particular student
 
-var courseName = new Array();
-courseName = ["CIS444", "CS351"];
-var subForumName = ["HW", "TEST", "QUIZ", "MISC"];
-var numPost = 0;
-var date = new Date();
-var dd = date.getDate() -1;
-var mm = date.getMonth() + 1;
-var yyyy = date.getFullYear();
-var today = mm + "-" + dd + "-" + yyyy;
+
 function writeTable(){
+	var courseName = new Array();
+	courseName = [["CIS444", ["HW" ,"4", "11-18-2020"],["TEST" ,"2", "11-18-2020"], ["QUIZ" ,"0", "11-18-2020"], ["OTHER/MISC" ,"0", "11-18-2020"]], 
+				["CS351", ["HW" ,"0", "11-18-2020"],["TEST" ,"0", "11-18-2020"], ["QUIZ" ,"0", "11-18-2020"], ["OTHER/MISC" ,"0", "11-18-2020"]]];
+
     //Case of not having any class
     if(courseName.length === 0){
 		var noclass = document.createElement("div");
@@ -41,18 +37,19 @@ function writeTable(){
         table_format_heading.appendChild(date_col);
 
 
-        for(let i = 0; i<courseName.length;i++){
+        for(let i = 0; i<courseName.length;i++) {
+			
             var table_format_courseName = document.createElement("div");
             table_format_courseName.setAttribute("class", "table-format");
             document.getElementById("forum-div").appendChild(table_format_courseName);
             var course_node = document.createElement("div");
             course_node.setAttribute("class", "course-title");
-            course_node.innerHTML = courseName[i];
+            course_node.innerHTML = courseName[i][0];
             table_format_courseName.appendChild(course_node);
-            for(let j = 0; j<subForumName.length;j++){
+			for (let j = 1; j < courseName[i].length; j++){
                 var table_format_subForum = document.createElement("div");
                 table_format_subForum.setAttribute("class", "table-format");
-				table_format_subForum.setAttribute("id", courseName[i]+"-" + subForumName[j]);
+				table_format_subForum.setAttribute("id", courseName[i][0]+"-" + courseName[i][j][0]);
                 document.getElementById("forum-div").appendChild(table_format_subForum);
                 var subForum_node = document.createElement("div");
                 subForum_node.setAttribute("class","item-forum item-category")
@@ -69,22 +66,21 @@ function writeTable(){
                 subForum_node.appendChild(sub_title_node);
                 var sub_heading_node = document.createElement("div");
                 sub_heading_node.setAttribute("class", "sub-heading");
-				var param = document.getElementById(courseName[i]+"-" + subForumName[j]);
+				var param = document.getElementById(courseName[i][0]+"-" + courseName[i][j][0]);
                 sub_heading_node.setAttribute("onclick", "goToSubforum('"+param.id+"');");
-                sub_heading_node.innerHTML = subForumName[j];
+                sub_heading_node.innerHTML = courseName[i][j][0];
                 sub_title_node.appendChild(sub_heading_node);
                 var sub_description_node = document.createElement("div");
                 sub_description_node.setAttribute("class", "sub-description small-font");
-                sub_description_node.innerHTML = "The courses and subforums displayed currently are dummy (but still dynamic) data so that" + 
-				" linkages to subforums, view post, and create post/reply are available.";
+                sub_description_node.innerHTML = "Click to view " + courseName[i][0] + " " + courseName[i][j][0] + "'s posts.";
                 sub_title_node.appendChild(sub_description_node);
                 var post_col_node = document.createElement("div");
                 post_col_node.setAttribute("class", "item-post");
-                post_col_node.innerHTML = "" + numPost;
+                post_col_node.innerHTML = "" + courseName[i][j][1];
                 table_format_subForum.appendChild(post_col_node);
                 var date_col_node = document.createElement("div");
                 date_col_node.setAttribute("class", "item-date");
-                date_col_node.innerHTML = today;
+                date_col_node.innerHTML = courseName[i][j][2];
                 table_format_subForum.appendChild(date_col_node);
             }
         }
